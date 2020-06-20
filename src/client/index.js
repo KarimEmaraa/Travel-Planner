624069ba204279e33802b8551325baf828e3a110
Karim Emara <karim.osa4@gmail.com>
2020-06-20 20:07:45 +0200

    Feat Added Card creator function

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,5 @@
+import { createCard } from './js/cardCreator'
+
 import './media/Travel Planner.svg';
+import './media/favorite-2_th.jpg';
+createCard("test", "test", "Test");
---
+++ src/client/js/cardCreator.js
@@ -0,0 +1,30 @@
+import ia from '../media/favorite-2_th.jpg';
+
+function createCard(dist, date, image) {
+    //create a virtual dom
+    const virtualDOM = document.createDocumentFragment();
+    //create a card div
+    const carddiv = document.createElement('div');
+    carddiv.classList = 'card';
+    //create an img
+    const img = document.createElement('img');
+    img.src = ia;
+    //create card text.
+    const textdiv = document.createElement('div');
+    textdiv.classList = 'card-body';
+    //create a p element
+    const cardp = document.createElement('p');
+    cardp.innerHTML = "text";
+    textdiv.appendChild(cardp);
+
+    carddiv.appendChild(img);
+    carddiv.appendChild(textdiv);
+    //add those to virtual dom.
+    virtualDOM.appendChild(carddiv);
+    //append to to maindom*/
+    const maincards = document.getElementsByClassName('plan-cards');
+    maincards[0].appendChild(virtualDOM);
+
+}
+
+export { createCard }
