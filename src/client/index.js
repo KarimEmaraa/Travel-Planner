ed19f357cf719c4de99f39b6bae7b9600cb7a317
Karim Emara <karim.osa4@gmail.com>
2020-06-22 02:35:36 +0200

    feat added sass styling fixed some html typos

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ import { submitHandler } from './js/submitHandler'
 import { fetchAPIData, updateMinDate, importAll, getTripDuration } from './js/utils';
 
 import './media/Travel Planner.svg';
+import './styles/style.scss';
 
 updateMinDate();
 document.getElementById('submitButton').addEventListener('click', submitHandler );
